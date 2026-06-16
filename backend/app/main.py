ab5837684696064a4da70bf568b01c321de1de82
musabirfan <22f23355@mec.edu.om>
2026-06-17 03:29:25 +0400

    CORS rearrangement

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,7 +22,11 @@ app = FastAPI(title="Smart Personal Finance Assistant API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://spfa-frontend.netlify.app"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://spfa-frontend.netlify.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
